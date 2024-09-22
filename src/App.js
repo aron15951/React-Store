86edaa2f9536ec515ca19b840ce418e44e1d78ad
aron15951 <aronchoque72@gmail.com>
2024-09-22 11:56:07 -0500

    start project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+
+function Home() {
+    return <h2 className="text-2xl">Inicio</h2>;
+}
+
+function About() {
+    return <h2 className="text-2xl">Acerca de</h2>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="p-4">
+            <nav className="mb-4">
+                <Link to="/" className="mr-4 text-blue-500">Inicio</Link>
+                <Link to="/about" className="text-blue-500">Acerca de</Link>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
